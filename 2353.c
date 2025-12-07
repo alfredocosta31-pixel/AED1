19cf07fd9607df3a4880c611b9351165957fc510
alfredocosta31-pixel <alfredo.costa31@unifesp.br>
2025-12-07 07:39:34 -0300

    Create 2353.c

---
+++ 2353.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include <math.h>
+
+#define true 1
+#define false 0
+
+long isPrime(long);
+
+int main ()
+{
+
+    long n;
+    scanf("%ld", &n);
+    printf("%ld\n", n == 2 ? 2 : isPrime(n));
+    
+}
+
+long isPrime(long x)
+{
+
+    long i, j;
+    if (x % 2 == 0)
+        x -= 1;
+
+    for (i = x; i >= 2; i -= 2)
+    {
+
+        _Bool flag = true;
+        long size = sqrt(i);
+        for (j = 3; j <= size; j += 2)
+            if (i % j == 0)
+            {
+                flag = false;
+                break;
+            }
+
+        if (flag)
+            return i;
+
+    }
+
+}
