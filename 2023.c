670f6acf652b48b73833a5fa948757135c3bcd6e
alfredocosta31-pixel <alfredo.costa31@unifesp.br>
2025-12-07 07:36:34 -0300

    Create 2023.c

---
+++ 2023.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct{
+
+	char nome[100];
+
+} lista;
+
+void ordena(lista *, unsigned short);
+
+int main ()
+{
+
+	lista criancas[1100];
+	unsigned short i, j;
+
+	i = 0;
+	while (scanf(" %[^\n]", criancas[i].nome) != EOF)
+		i++;
+
+	ordena(criancas, i);
+
+	printf("%s\n", criancas[i - 1].nome);
+
+}
+
+void ordena(lista *criancas, unsigned short tam)
+{
+
+	short i = 1, j;
+	lista pivo;
+
+	while (i < tam)
+	{
+
+		j = i - 1;
+		pivo = criancas[i];
+
+		while (j >= 0 && strcasecmp(criancas[j].nome, pivo.nome) > 0)
+		{
+
+			criancas[j + 1] = criancas[j];
+			j--;
+
+		}
+
+		criancas[j + 1] = pivo;
+		i++;
+
+	}
+
+}
